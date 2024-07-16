564cde883db78c961624acf05881e10779826a13
Joshua Enahoro <joshuaenahoro@gmail.com>
2024-07-16 14:49:18 +0100

    setup header

--- src/app/(dashboard)/cards/[accountNum]/header.tsx
+++ src/app/(dashboard)/cards/[accountNum]/header.tsx
@@ -1,8 +1,23 @@
+import { BackButton } from '@/components/back-button';
+import { Badge } from '@/components/badge';
+import { Button } from '@/components/button';
+
 export function Header() {
   return (
-    <header>
+    <header className="mb-5 border-b border-[#E4E7EC] pb-5">
+      <div className="mb-8 flex justify-between">
+        <BackButton />
+
+        <div className="flex items-center gap-2">
+          <Badge variant="green" hasBullet>
+            Active
+          </Badge>
+          <Button variant="outline">Freeze card</Button>
+          <Button variant="destructive">Block card</Button>
+        </div>
+      </div>
+
       <h1 className="text-lg font-semibold">Card info</h1>
-      <p className="text-[#475467]">See details on the card here.</p>
     </header>
   );
 }
