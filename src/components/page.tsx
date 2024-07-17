0cfa944f67e981806f01c44219f05de04d1ae477
Joshua Enahoro <joshuaenahoro@gmail.com>
2024-07-17 09:14:27 +0100

    cleanup

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -19,20 +19,15 @@ export function PageHeader({
 
 export function PageHeaderWithActionItem({
   heading,
-  subheading,
   children,
 }: {
   heading: string;
-  subheading: string;
   children: React.ReactNode;
 }) {
   return (
-    <header className="mb-8 flex justify-between border-b border-[#E4E7EC] pb-8">
-      <div>
-        <h1 className="text-lg font-semibold">{heading}</h1>
-        <p className="text-sm text-[#475467]">{subheading}</p>
-      </div>
-      {children}
+    <header className="mb-8 flex items-center justify-between border-b border-[#E4E7EC] pb-8">
+      <h1 className="text-lg font-semibold text-[#101828]">{heading}</h1>
+      <div className="flex gap-4">{children}</div>
     </header>
   );
 }
