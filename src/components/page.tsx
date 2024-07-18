761ccc863d52e85e25a069a12b2e627993679992
Joshua Enahoro <joshuaenahoro@gmail.com>
2024-07-18 06:27:21 +0100

    refactor properties

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -1,17 +1,46 @@
+import { cva, VariantProps } from 'class-variance-authority';
+import { twJoin } from 'tailwind-merge';
+
 export function PageWrapper({ children }: { children: React.ReactNode }) {
   return <div className="rounded-2xl bg-white p-8">{children}</div>;
 }
 
+const pageHeader = cva(
+  'mb-8 flex flex-col gap-8 border-b border-[#E4E7EC] pb-8',
+  {
+    variants: {
+      variant: {
+        default: 'items-center',
+        secondary: '',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+    },
+  },
+);
+
+interface PageHeaderProps extends VariantProps<typeof pageHeader> {
+  heading: string;
+  children?: React.ReactNode;
+}
+
 export function PageHeader({
+  variant = 'default',
   heading,
   children,
-}: {
-  heading: string;
-  children: React.ReactNode;
-}) {
+}: PageHeaderProps) {
   return (
-    <header className="mb-8 flex flex-col items-center gap-8 border-b border-[#E4E7EC] pb-8">
-      <h1 className="text-3xl font-[600] text-[#101828]">{heading}</h1>
+    <header className={pageHeader({ variant })}>
+      <h1
+        className={twJoin(
+          'font-semibold text-[#101828]',
+          variant === 'default' && 'text-3xl',
+          variant === 'secondary' && 'text-lg',
+        )}
+      >
+        {heading}
+      </h1>
       {children}
     </header>
   );
