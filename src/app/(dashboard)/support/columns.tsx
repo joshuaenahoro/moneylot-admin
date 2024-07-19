3d83e133e05c53c891fcbee3101044b6e53b30d1
Joshua Enahoro <joshuaenahoro@gmail.com>
2024-07-19 11:40:15 +0100

    set up support page

---
+++ src/app/(dashboard)/support/columns.tsx
@@ -0,0 +1,176 @@
+'use client';
+
+import { Icon } from '@/components/icon';
+import { ColumnDef } from '@tanstack/react-table';
+import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/tooltip';
+import Link from 'next/link';
+import { Badge, BadgeProps } from '@/components/badge';
+
+export type Support = {
+  customer: string;
+  ticketID: string;
+  transactionID: string;
+  datetime: string;
+  type:
+    | 'Transfer'
+    | 'Airtime'
+    | 'Data'
+    | 'Education'
+    | 'Internet services'
+    | 'Betting';
+  status: 'Requested' | 'Completed' | 'Processing' | 'Pending';
+  admin: string;
+};
+
+export const columns: ColumnDef<Support>[] = [
+  {
+    accessorKey: 'customer',
+    header: ({ column }) => {
+      return (
+        <button
+          className="flex gap-2 text-left"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Customer
+          <Icon name="arrows-up-down" className="h-4 w-4" />
+        </button>
+      );
+    },
+    cell: ({ row }) => (
+      <div className="text-[#232323]">{`${row.getValue('customer')}`}</div>
+    ),
+  },
+  {
+    accessorKey: 'ticketID',
+    header: ({ column }) => {
+      return (
+        <button
+          className="flex gap-2 text-left"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Ticket ID
+          <Icon name="arrows-up-down" className="h-4 w-4" />
+        </button>
+      );
+    },
+    cell: ({ row }) => (
+      <div className="text-[#232323]">{`${row.getValue('ticketID')}`}</div>
+    ),
+  },
+  {
+    accessorKey: 'transactionID',
+    header: ({ column }) => {
+      return (
+        <button
+          className="flex gap-2 text-left"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Transaction ID
+          <Icon name="arrows-up-down" className="h-4 w-4" />
+        </button>
+      );
+    },
+    cell: ({ row }) => (
+      <div className="text-[#232323]">{`${row.getValue('transactionID')}`}</div>
+    ),
+  },
+  {
+    accessorKey: 'datetime',
+    header: ({ column }) => {
+      return (
+        <button
+          className="flex gap-2 text-left"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Date and time
+          <Icon name="arrows-up-down" className="h-4 w-4" />
+        </button>
+      );
+    },
+    cell: ({ row }) => (
+      <div className="text-[#475467]">{`${row.getValue('datetime')}`}</div>
+    ),
+  },
+  {
+    accessorKey: 'type',
+    header: ({ column }) => {
+      return (
+        <button
+          className="flex gap-2 text-left"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Type
+          <Icon name="arrows-up-down" className="h-4 w-4" />
+        </button>
+      );
+    },
+    cell: ({ row }) => (
+      <div className="text-[#475467]">{`${row.getValue('type')}`}</div>
+    ),
+  },
+  {
+    accessorKey: 'status',
+    header: ({ column }) => {
+      return (
+        <button
+          className="flex gap-2 text-left"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Status
+          <Icon name="arrows-up-down" className="h-4 w-4" />
+        </button>
+      );
+    },
+    cell: ({ row }) => {
+      let variant = 'default' as BadgeProps['variant'];
+      switch (row.getValue('status')) {
+        case 'Requested':
+          variant = 'purple';
+          break;
+        case 'Completed':
+          variant = 'green';
+          break;
+        case 'Pending':
+          variant = 'orange';
+          break;
+      }
+      return (
+        <Badge variant={variant} hasBullet>{`${row.getValue('status')}`}</Badge>
+      );
+    },
+  },
+  {
+    accessorKey: 'admin',
+    header: ({ column }) => {
+      return (
+        <button
+          className="flex gap-2 text-left"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Admin
+          <Icon name="arrows-up-down" className="h-4 w-4" />
+        </button>
+      );
+    },
+    cell: ({ row }) => (
+      <div className="text-[#232323]">{`${row.getValue('admin')}`}</div>
+    ),
+  },
+  {
+    id: 'action',
+    cell: () => {
+      return (
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Link href="/savings-list/1093992823">
+              <Icon name="chevron-right" className="h-4 w-4" />
+            </Link>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>See target details</p>
+          </TooltipContent>
+        </Tooltip>
+      );
+    },
+  },
+];
--- src/app/(dashboard)/support/page.tsx
+++ src/app/(dashboard)/support/page.tsx
@@ -1,20 +1,164 @@
+import { DataTable } from '@/components/data-table';
 import { PageHeader, PageWrapper } from '@/components/page';
 import { SearchBar } from '@/components/search-bar';
 import { SITE_NAME } from '@/constants';
 import { Metadata } from 'next';
+import { columns, Support } from './columns';
 
 export const metadata: Metadata = {
   title: `Support - ${SITE_NAME}`,
   description: '',
 };
 
-export default function SupportPage() {
+async function getData(): Promise<Support[]> {
+  return [
+    {
+      customer: 'Samantha Jones',
+      ticketID: '978067643197-1',
+      transactionID: '978067643197-2',
+      datetime: ' 28/1/2024 - 12:45 pm',
+      type: 'Transfer',
+      status: 'Requested',
+      admin: '@Johndoe93',
+    },
+    {
+      customer: 'Samantha Jones',
+      ticketID: '978067643197-1',
+      transactionID: '978067643197-2',
+      datetime: ' 28/1/2024 - 12:45 pm',
+      type: 'Transfer',
+      status: 'Completed',
+      admin: '@Johndoe93',
+    },
+    {
+      customer: 'Samantha Jones',
+      ticketID: '978067643197-1',
+      transactionID: '978067643197-2',
+      datetime: ' 28/1/2024 - 12:45 pm',
+      type: 'Transfer',
+      status: 'Processing',
+      admin: '@Johndoe93',
+    },
+    {
+      customer: 'Samantha Jones',
+      ticketID: '978067643197-1',
+      transactionID: '978067643197-2',
+      datetime: ' 28/1/2024 - 12:45 pm',
+      type: 'Transfer',
+      status: 'Pending',
+      admin: '@Johndoe93',
+    },
+    {
+      customer: 'Samantha Jones',
+      ticketID: '978067643197-1',
+      transactionID: '978067643197-2',
+      datetime: ' 28/1/2024 - 12:45 pm',
+      type: 'Transfer',
+      status: 'Completed',
+      admin: '@Johndoe93',
+    },
+    {
+      customer: 'Samantha Jones',
+      ticketID: '978067643197-1',
+      transactionID: '978067643197-2',
+      datetime: ' 28/1/2024 - 12:45 pm',
+      type: 'Transfer',
+      status: 'Completed',
+      admin: '@Johndoe93',
+    },
+    {
+      customer: 'Samantha Jones',
+      ticketID: '978067643197-1',
+      transactionID: '978067643197-2',
+      datetime: ' 28/1/2024 - 12:45 pm',
+      type: 'Transfer',
+      status: 'Completed',
+      admin: '@Johndoe93',
+    },
+    {
+      customer: 'Samantha Jones',
+      ticketID: '978067643197-1',
+      transactionID: '978067643197-2',
+      datetime: ' 28/1/2024 - 12:45 pm',
+      type: 'Transfer',
+      status: 'Completed',
+      admin: '@Johndoe93',
+    },
+    {
+      customer: 'Samantha Jones',
+      ticketID: '978067643197-1',
+      transactionID: '978067643197-2',
+      datetime: ' 28/1/2024 - 12:45 pm',
+      type: 'Transfer',
+      status: 'Completed',
+      admin: '@Johndoe93',
+    },
+    {
+      customer: 'Samantha Jones',
+      ticketID: '978067643197-1',
+      transactionID: '978067643197-2',
+      datetime: ' 28/1/2024 - 12:45 pm',
+      type: 'Transfer',
+      status: 'Completed',
+      admin: '@Johndoe93',
+    },
+    {
+      customer: 'Samantha Jones',
+      ticketID: '978067643197-1',
+      transactionID: '978067643197-2',
+      datetime: ' 28/1/2024 - 12:45 pm',
+      type: 'Transfer',
+      status: 'Completed',
+      admin: '@Johndoe93',
+    },
+    {
+      customer: 'Samantha Jones',
+      ticketID: '978067643197-1',
+      transactionID: '978067643197-2',
+      datetime: ' 28/1/2024 - 12:45 pm',
+      type: 'Transfer',
+      status: 'Completed',
+      admin: '@Johndoe93',
+    },
+    {
+      customer: 'Samantha Jones',
+      ticketID: '978067643197-1',
+      transactionID: '978067643197-2',
+      datetime: ' 28/1/2024 - 12:45 pm',
+      type: 'Transfer',
+      status: 'Completed',
+      admin: '@Johndoe93',
+    },
+    {
+      customer: 'Samantha Jones',
+      ticketID: '978067643197-1',
+      transactionID: '978067643197-2',
+      datetime: ' 28/1/2024 - 12:45 pm',
+      type: 'Transfer',
+      status: 'Completed',
+      admin: '@Johndoe93',
+    },
+    {
+      customer: 'Samantha Jones',
+      ticketID: '978067643197-1',
+      transactionID: '978067643197-2',
+      datetime: ' 28/1/2024 - 12:45 pm',
+      type: 'Transfer',
+      status: 'Completed',
+      admin: '@Johndoe93',
+    },
+  ];
+}
+
+export default async function SupportPage() {
+  const data = await getData();
+
   return (
     <PageWrapper>
       <PageHeader heading="Support">
         <SearchBar />
       </PageHeader>
-      <h1 className="sr-only">Support</h1>
+      <DataTable columns={columns} data={data} />
     </PageWrapper>
   );
 }
