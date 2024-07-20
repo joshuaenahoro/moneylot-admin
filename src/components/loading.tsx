ec10c7c1d19275fcae87ad18a0cc09fecaadd5ac
Joshua Enahoro <joshuaenahoro@gmail.com>
2024-07-20 19:56:27 +0100

    feature: use spinner component

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,20 +1,10 @@
+import { Spinner } from './spinner';
+
 export function Loading() {
   return (
     <div className="fixed inset-0 z-30 grid bg-[#000000]/50">
       <div className="sr-only">Loading</div>
-      <svg
-        width={86}
-        height={86}
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="white"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth={2}
-        className="m-auto animate-spin"
-      >
-        <path d="M21 12a9 9 0 1 1-6.219-8.56" />
-      </svg>
+      <Spinner size={86} />
     </div>
   );
 }
