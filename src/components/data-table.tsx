c75d7ef0c91328b76bef06826617cd2d939f0149
Joshua Enahoro <joshuaenahoro@gmail.com>
2024-07-21 17:25:41 +0100

    refactor: cleanup import and pagination

--- src/components/data-table.tsx
+++ src/components/data-table.tsx
@@ -19,7 +19,6 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/table';
-import { Button } from '@/components/button';
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -88,30 +87,6 @@ export function DataTable<TData, TValue>({
           )}
         </TableBody>
       </Table>
-      {/* <div className="flex items-center justify-end space-x-2 py-4">
-        <div className="text-muted-foreground flex-1 text-sm">
-          {table.getFilteredSelectedRowModel().rows.length} of{' '}
-          {table.getFilteredRowModel().rows.length} row(s) selected.
-        </div>
-        <div className="space-x-2">
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.previousPage()}
-            disabled={!table.getCanPreviousPage()}
-          >
-            Previous
-          </Button>
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.nextPage()}
-            disabled={!table.getCanNextPage()}
-          >
-            Next
-          </Button>
-        </div>
-      </div> */}
     </div>
   );
 }
