0fada7bcc38cb404660012f271c1c6c7e5cc563b
Joshua Enahoro <joshuaenahoro@gmail.com>
2024-07-22 13:22:11 +0100

    feature: setup actions

--- src/app/(dashboard)/admin-role-management/columns.tsx
+++ src/app/(dashboard)/admin-role-management/columns.tsx
@@ -1,15 +1,22 @@
 'use client';
 
 import { Icon } from '@/components/icon';
+import { Loading } from '@/components/loading';
 import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/tooltip';
 import { ColumnDef } from '@tanstack/react-table';
 import Link from 'next/link';
+import { useState } from 'react';
+import { toast } from 'sonner';
 
 export type AdminRoles = {
   id: string;
+  user: {
+    name: string;
+    email: string;
+  };
+  accountNum: number;
   role: string;
-  description: string;
-  entryDatetime: string;
+  assignmentDatetime: string;
 };
 
 export const columns: ColumnDef<AdminRoles>[] = [
@@ -31,7 +38,7 @@ export const columns: ColumnDef<AdminRoles>[] = [
     ),
   },
   {
-    accessorKey: 'role',
+    accessorKey: 'user',
     header: ({ column }) => {
       return (
         <button
@@ -43,59 +50,104 @@ export const columns: ColumnDef<AdminRoles>[] = [
         </button>
       );
     },
-    cell: ({ row }) => (
-      <div className="text-[#101828]">{row.getValue('role')}</div>
-    ),
+    cell: ({ row }) => {
+      const user: { name: string; email: string } = row.getValue('user');
+      return (
+        <div>
+          <div className="text-[#232323]">{user.name}</div>
+          <div className="text-xs text-dark-50">{user.email}</div>
+        </div>
+      );
+    },
   },
   {
-    accessorKey: 'description',
+    accessorKey: 'accountNum',
     header: ({ column }) => {
       return (
         <button
           className="flex gap-2 text-left"
           onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
         >
-          Description
+          Account number
           <Icon name="arrows-up-down" className="h-4 w-4" />
         </button>
       );
     },
     cell: ({ row }) => (
-      <div className="text-[#101828]">{row.getValue('description')}</div>
+      <div className="text-[#101828]">{row.getValue('accountNum')}</div>
     ),
   },
   {
-    accessorKey: 'entryDatetime',
+    accessorKey: 'role',
+    header: 'User role',
+    cell: ({ row }) => (
+      <div className="text-[#475467]">{row.getValue('role')}</div>
+    ),
+  },
+  {
+    accessorKey: 'assignmentDatetime',
     header: ({ column }) => {
       return (
         <button
           className="flex gap-2 text-left"
           onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
         >
-          Entry date & time
+          Assignment date & time
           <Icon name="arrows-up-down" className="h-4 w-4" />
         </button>
       );
     },
     cell: ({ row }) => (
-      <div className="text-[#101828]">{row.getValue('entryDatetime')}</div>
+      <div className="text-[#101828]">{row.getValue('assignmentDatetime')}</div>
     ),
   },
   {
-    id: 'action',
+    id: 'actions',
+    header: 'Actions',
     cell: () => {
+      const [isActionRunning, setIsActionRunning] = useState(false);
+
+      function handleArchive() {
+        setIsActionRunning(true);
+        setTimeout(() => {
+          setIsActionRunning(false);
+          toast.success('Admin archived');
+        }, 1000);
+      }
+
+      function handleRefresh() {
+        setIsActionRunning(true);
+        setTimeout(() => {
+          setIsActionRunning(false);
+          toast.success('Login refreshed');
+        }, 1000);
+      }
+
       return (
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <Link href="/accounts/xy">
-              <span className="sr-only">User role details</span>
-              <Icon name="edit" className="h-4 w-4 text-[#5371FF]" />
-            </Link>
-          </TooltipTrigger>
-          <TooltipContent>
-            <p>Edit user role</p>
-          </TooltipContent>
-        </Tooltip>
+        <div className="flex gap-9">
+          {isActionRunning && <Loading />}
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Link href="/admin-role-management/edit">
+                <span className="sr-only">Edit admin role</span>
+                <Icon name="edit" className="h-6 w-6 text-[#5371FF]" />
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent>Edit admin role</TooltipContent>
+          </Tooltip>
+          <Tooltip>
+            <TooltipTrigger onClick={handleArchive}>
+              <Icon name="archive" className="h-6 w-6 text-[#5371FF]" />
+            </TooltipTrigger>
+            <TooltipContent>Archive</TooltipContent>
+          </Tooltip>
+          <Tooltip>
+            <TooltipTrigger onClick={handleRefresh}>
+              <Icon name="replay" className="h-6 w-6 text-[#5371FF]" />
+            </TooltipTrigger>
+            <TooltipContent>Refresh login</TooltipContent>
+          </Tooltip>
+        </div>
       );
     },
   },
