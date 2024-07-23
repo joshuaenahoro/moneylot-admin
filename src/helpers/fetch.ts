7f5b75a1ed75ff93d65ae10ff5a6d529f8656ecd
Joshua Enahoro <joshuaenahoro@gmail.com>
2024-07-23 16:25:18 +0100

    feature: create fetcher helper

---
+++ src/helpers/fetch.ts
@@ -0,0 +1,16 @@
+export async function postData(url = '', data = {}) {
+  const response = await fetch(process.env.API_BASE_URL + url, {
+    method: 'POST',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+    body: JSON.stringify(data),
+  });
+
+  return response.json();
+}
