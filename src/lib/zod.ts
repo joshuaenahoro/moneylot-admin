69fcb6b7bc0cc57501814430d7dd8382e6a7c69d
Joshua Enahoro <joshuaenahoro@gmail.com>
2024-08-21 05:26:05 +0100

    refactor: move signin schema to lib

--- src/lib/zod.ts
+++ src/lib/zod.ts
@@ -1,10 +1,8 @@
 import { z } from 'zod';
 
-export const FormSchema = z.object({
+export const signInSchema = z.object({
   email: z.string().email(),
   password: z
     .string()
     .min(2, { message: 'Password must be at least 2 characters' }),
 });
-
-export type FormData = z.infer<typeof FormSchema>;
