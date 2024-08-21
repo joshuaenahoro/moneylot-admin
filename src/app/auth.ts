a400eb3ae2500b8d28e34012ab921016e6dae2e1
Joshua Enahoro <joshuaenahoro@gmail.com>
2024-08-21 05:25:40 +0100

    feat: setup auth

---
+++ src/app/auth.ts
@@ -0,0 +1,64 @@
+import { postData } from '@/helpers/fetch';
+import { signInSchema } from '@/lib/zod';
+import NextAuth, { AuthError, User } from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import { ZodError } from 'zod';
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        try {
+          let user = null;
+
+          // validate schema
+          const validationResult = signInSchema.safeParse(credentials);
+          if (!validationResult.success) {
+            return {
+              errors: validationResult.error.flatten().fieldErrors,
+            };
+          }
+
+          const { email, password } =
+            await signInSchema.parseAsync(credentials);
+
+          // verify if the user exists
+          user = await postData('/api/v1/auth/authenticate', {
+            email,
+            password,
+          });
+
+          if (!user) {
+            throw new Error('User not found.');
+          }
+
+          console.log('user:', user);
+          // return JSON object with the user data
+          return user;
+        } catch (error) {
+          if (error instanceof AuthError) {
+            switch (error.type) {
+              case 'CredentialsSignin':
+                return { error: 'Invalid credentials' };
+              default:
+                return { error: 'Something went wrong' };
+            }
+          }
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    authorized: async ({ auth }) => {
+      // Logged in users are authenticated, otherwise redirect to login page
+      return !!auth;
+    },
+  },
+  pages: {
+    signIn: '/login',
+  },
+});
