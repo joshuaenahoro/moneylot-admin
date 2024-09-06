f70578d56066a69a206424e078816cebae367a84
Joshua Enahoro <joshuaenahoro@gmail.com>
2024-09-06 07:52:57 +0100

    feat: add site manifest to middleware exceptions

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,7 @@
 export { auth as middleware } from '@/app/auth';
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: [
+    '/((?!api|_next/static|_next/image|site.webmanifest|favicon.ico).*)',
+  ],
 };
