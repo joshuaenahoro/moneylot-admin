897ce62bb3777e8d9994c120a2d5b9ce36765862
Joshua Enahoro <joshuaenahoro@gmail.com>
2024-09-17 08:12:37 +0100

    feat: add getData fn

--- src/helpers/fetch.ts
+++ src/helpers/fetch.ts
@@ -14,3 +14,18 @@ export async function postData(url = '', data = {}) {
 
   return response.json();
 }
+
+export async function getData(url = '') {
+  const response = await fetch(process.env.API_BASE_URL + url, {
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+  });
+
+  return response.json();
+}
